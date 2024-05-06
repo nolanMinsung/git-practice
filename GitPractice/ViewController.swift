e5b2ab398e7e36d55096c3da3f1859d6af0f2d70
Minsung Kim <mskim4048@naver.com>
2024-05-06 17:30:15 +0900

    Initial Commit

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by 김민성 on 2024/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
