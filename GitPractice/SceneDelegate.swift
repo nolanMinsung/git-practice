c5151bc490b3f146b0e94b85082bc045b71f75f7
Minsung Kim <mskim4048@naver.com>
2024-05-26 17:55:00 +0900

    window의 backgroundColor 세팅

--- GitPractice/SceneDelegate.swift
+++ GitPractice/SceneDelegate.swift
@@ -20,6 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         self.window = UIWindow(windowScene: windowScene)
         self.window?.rootViewController = ViewController()
+        self.window?.backgroundColor = .systemBackground
         self.window?.makeKeyAndVisible()
     }
 
